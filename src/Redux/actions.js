c7e78bda79e379ca16b736ccce33236d1f2b750e
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2022-08-17 03:45:16 +0530

    Update actions.js

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -36,7 +36,7 @@ const locationFailure = () => ({
 export const locationData = () => (dispatch) => {
   dispatch(locationReq());
   axios
-    .get("https://ip-api.com/json/")
+    .get("http://ip-api.com/json/")
     .then((res) => dispatch(locationSuccess(res.data)))
     .catch(() => dispatch(locationFailure()));
 };
